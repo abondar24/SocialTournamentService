3aef0e4e2c590d5db5e58e57be8784f6a1811f51
abondar24 <abondar1992@gmail.com>
2018-03-19 20:56:46 +0100

    Refactoring, added bootstrap

---
+++ admin_frontend/src/components/Main.vue
@@ -0,0 +1,40 @@
+<template src="../templates/main.html"></template>
+
+<script>
+export default {
+  name: 'Main',
+  props: {
+    msg: String,
+    statusCode: Number,
+    serverStatusOk: {
+        default: true,
+        type: Boolean
+    },
+  },
+
+    methods: {
+        status: function () {
+            this.$http.get(this.$hostname+'/status')
+                .then(response => {
+                    console.log(response);
+                this.msg = response.data.msg;
+                this.statusCode = response.data.code;
+                    if (this.statusCode!==200){
+                        this.serverStatusOk = false;
+                    }
+        },error=>{
+                    this.msg = error.data;
+                    this.statusCode = error.statusCode;
+                    this.serverStatusOk = false;
+                });
+        },
+
+    },
+    created: function () {
+        this.status();
+    }
+}
+</script>
+
+<style src="../styles/main.css">
+</style>
--- admin_frontend/src/main.js
+++ admin_frontend/src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
+import BootstrapVue from 'bootstrap-vue'
 
 Vue.config.productionTip = false;
 Vue.use(VueResource);
+Vue.use(BootstrapVue);
+
+Vue.prototype.$hostname =  "http://"+window.location.hostname+":"+window.location.port;
+
 new Vue({
   render: h => h(App)
 }).$mount('#app');
